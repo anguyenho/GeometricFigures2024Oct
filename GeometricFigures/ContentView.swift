3fb24a06927081242d7ee77d76ad809981354010
Ainsley Nguyenho <anguyenho@lcs.on.ca>
2024-10-03 09:42:05 -0400

    Initial Commit

---
+++ GeometricFigures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometricFigures
+//
+//  Created by Ainsley Quyen Nguyenho on 2024-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
